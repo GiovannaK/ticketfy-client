c00386c9f7b5aa0d69ab6a31320184060f74d1e1
Giovanna Cunha <giovannakelli@gmail.com>
2021-10-29 03:48:30 -0300

    navbar was styled

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,27 +1,44 @@
 import React from 'react'
 import MenuIcon from '@mui/icons-material/Menu';
 import { Box } from '@mui/system';
-import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
-import { AppBarStyled } from './styles';
+import { Hidden, IconButton, Toolbar } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import { AppBarStyled, LoginButton, RegisterButton } from './styles';
+import SellIcon from '@mui/icons-material/Sell';
 
 export const Navbar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBarStyled position="fixed">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon sx={{fontSize: 40}}/>
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Hidden mdUp>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon sx={{fontSize: 35}}/>
+            </IconButton>
+          </Hidden>
+          <Box sx={{flexGrow: 1}}>
+            <img src="logo.png" style={{maxWidth: 115}}/>
+          </Box>
+          <Hidden mdDown>
+            <RegisterButton
+              variant="outlined"
+              startIcon={<SellIcon />}
+            >
+               Quero vender
+            </RegisterButton>
+            <LoginButton
+              startIcon={<PersonIcon />}
+              variant="contained"
+            >
+              Entrar
+            </LoginButton>
+          </Hidden>
         </Toolbar>
       </AppBarStyled>
     </Box>
